2f627b8f93dd9131aa187a79ac8d0e4e78708fe5
sydneycasey <casey.sydney@gmail.com>
2019-05-08 20:04:11 -0700

    displaying data from db

--- client/src/components/Jobs.jsx
+++ client/src/components/Jobs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Form extends React.Component {
+class Jobs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -10,17 +10,42 @@ class Form extends React.Component {
 
   componentDidMount() {
     fetch('/jobs')
-    .then(res => res.json())
-    .then(jobs => this.setState({ jobs }))
-    .catch(error => console.log(error));
+      .then(res => res.json())
+      .then(jobs => this.setState({ jobs }))
+      .catch(error => console.log(error));
   }
 
   render() {
     return (
-      this.state.jobs.map((job) => (
-        <div id="job">{job}</div>
-      ))
+      <table id="jobs-table">
+        <thead>
+          <tr id="headers">
+            <th id="company">Company</th>
+            <th id="applied">Applied</th>
+            <th id="sent_app">Application Date</th>
+            <th id="outreach">Outreach Sent</th>
+            <th id="received">Callback Received</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.jobs.map((job) => (
+            <tr id="jobs-list">
+              <td id="company">{job.company}</td>
+              <td id="applied">{job.applied ? 'yes' : 'no'}</td>
+              <td id="sent_app">{job.sent_app === null ? 'N/A' :
+                job.sent_app
+              }</td>
+              <td id="outreach">{job.outreach ? 'yes' : 'no'}</td>
+              <td id="sent_email">{job.sent_email}</td>
+              <td id="callback">{job.callback ? 'yes' : 'no'}</td>
+              <td id="received">{job.received}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     )
   }
 
 }
+
+export default Jobs;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form.jsx';
+import Jobs from './components/Jobs.jsx';
 
 const App = () => {
   return (
+    <div>
     <Form />
+    <Jobs />
+    </div>
   )
 }
 
